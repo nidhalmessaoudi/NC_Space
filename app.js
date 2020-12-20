3b5f1fe47b566db881968e03c986f7edb95264b9
unknown <nidhalmessaoudi@hotmail.com>
2020-12-20 21:55:39 +0100

    Backend Development and Configuration

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -11,13 +15,67 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/DatabaseNameDB", {useNewUrlParser: true, useUnifiedTopology: true});
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(process.env.DB_HOST, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
+
+const adminSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+adminSchema.plugin(passportLocalMongoose);
+
+const Admin = new mongoose.model("Admin", adminSchema);
+
+passport.use(Admin.createStrategy());
+ 
+passport.serializeUser(Admin.serializeUser());
+passport.deserializeUser(Admin.deserializeUser());
 
 app.get("/", function(req, res){
   res.render("home");
 });
 
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+app.get("/dashboard", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("dashboard");
+  } else {
+    res.redirect("/login");
+  }
+})
+
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
+})
+
+app.post("/login", function (req, res) {
+  const admin = new Admin( {
+    username: req.body.username,
+    password: req.body.password
+  });
+  req.login(admin, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/dashboard");
+      })
+    }
+  })
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
